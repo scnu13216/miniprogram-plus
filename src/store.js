a4aa1d8594609141b978e2828a5e79aa582bf492
罗伟锋 <374845414@qq.com>
2022-08-20 17:57:26 +0800

    恢复

--- src/store.js
+++ src/store.js
@@ -17,17 +17,16 @@ class Store {
             let target_property = app.store.mutations[key](value);
             // 更新仓库之后，检查 component_stack 里面被检查出与仓库数据有关的计算属性
             for (const key in this.component_stack) {
-                
-                // const component = this.component_stack[key]
-                // if (component.hasOwnProperty('computed_obj')) {
-                //     // 检查启用计算属性
-                //     if (component.computed_obj.hasOwnProperty(target_property)) {
-                //         // 执行更新
-                //         component.computed_obj[target_property].forEach(v => {
-                //             component[v]()
-                //         })
-                //     }
-                // }
+                const component = this.component_stack[key]
+                if (component.hasOwnProperty('computed_obj')) {
+                    // 检查启用计算属性
+                    if (component.computed_obj.hasOwnProperty(target_property)) {
+                        // 执行更新
+                        component.computed_obj[target_property].forEach(v => {
+                            component[v]()
+                        })
+                    }
+                }
             }
         }
     }
